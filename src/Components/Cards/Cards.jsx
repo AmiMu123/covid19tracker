42c472a6cda3585f1f8ba5ff3b31fd242c0e1658
Amir M <user@Amir.local>
2020-05-10 07:49:08 +0300

    create initial UI component

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Card = () =>{
+    return(
+        <h1>Cards</h1>
+    )
+}
+export default Card;
---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+export const fetchData = async () =>{
+    
+    try {
+        //short way es6 destrucureing and short object property method 
+        const {data:{confirmed,recovered,death,lastUpdate}} = await axios.get(url);
+        const modifiedData = {confirmed,recovered,death,lastUpdate};
+        
+        
+            //long way with es5
+            //const {data} = await axios.get(url);
+            // const modifiedData = {
+        //     confirmed:data.confirmed,
+        //     recovered:data.recovered,
+        //     death:data.death,
+        //     lastUpdate:data.lastUpdate,
+
+        // }
+        return modifiedData;
+        
+    } catch (error) {
+        
+    }
+}
