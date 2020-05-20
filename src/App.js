b4d29efb7dc8519b9f03d545c8944f6beb93cd2e
Amir M <user@Amir.local>
2020-05-20 12:37:43 +0300

    added some style elements to the card components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import styles from './App.module.css';
 import {fetchData} from './api'
 
 class App extends React.Component{
+    
         state={ data:{}}
+        
      async componentDidMount(){
          const fetchedData = await fetchData();
          this.setState({data:fetchedData})
@@ -15,6 +17,7 @@ class App extends React.Component{
     render(){
         const {data} = this.state;
         return(
+            
             <div className = {styles.container}>
                 <Cards data = {data}/>
                 <Chart />
--- src/api/index.js
+++ src/api/index.js
@@ -6,8 +6,8 @@ export const fetchData = async () =>{
     
     try {
         //short way es6 destrucureing and short object property method 
-        const {data:{confirmed,recovered,death,lastUpdate}} = await axios.get(url);
-        const modifiedData = {confirmed,recovered,death,lastUpdate};
+        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
+        const modifiedData = {confirmed,recovered,deaths,lastUpdate};
         
         
             //long way with es5
