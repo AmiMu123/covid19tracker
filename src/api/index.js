e0449edc454e109f6a90dec46463e0abe6c9ff00
AmiMu123 <amirmustefa707@gmail.com>
2020-05-22 14:40:06 +0300

    implemented a chart component

--- src/api/index.js
+++ src/api/index.js
@@ -1,27 +1,36 @@
+import axios from "axios";
 
-import axios from 'axios';
+const url = "https://covid19.mathdro.id/api";
+export const fetchData = async () => {
+  try {
+    //short way es6 destrucureing and short object property method
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+    const modifiedData = { confirmed, recovered, deaths, lastUpdate };
 
-const url = 'https://covid19.mathdro.id/api';
-export const fetchData = async () =>{
-    
-    try {
-        //short way es6 destrucureing and short object property method 
-        const {data:{confirmed,recovered,deaths,lastUpdate}} = await axios.get(url);
-        const modifiedData = {confirmed,recovered,deaths,lastUpdate};
-        
-        
-            //long way with es5
-            //const {data} = await axios.get(url);
-            // const modifiedData = {
-        //     confirmed:data.confirmed,
-        //     recovered:data.recovered,
-        //     death:data.death,
-        //     lastUpdate:data.lastUpdate,
+    //long way with es5
+    //const {data} = await axios.get(url);
+    // const modifiedData = {
+    //     confirmed:data.confirmed,
+    //     recovered:data.recovered,
+    //     death:data.death,
+    //     lastUpdate:data.lastUpdate,
 
-        // }
-        return modifiedData;
-        
-    } catch (error) {
-        
-    }
-}
+    // }
+    return modifiedData;
+  } catch (error) {}
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+
+    return modifiedData;
+  } catch (error) {}
+};
